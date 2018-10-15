0fd2ac567c4a062ddca1faf7ded56c2246f9f406
weiwei1988 <zhaoweiheng8828@gmail.com>
2018-10-16 00:19:08 +0900

    Change Triangle

--- Triangle.py
+++ Triangle.py
@@ -1,15 +1,22 @@
 def solution(A):
-    A = A.sort()
+    N = len(A)
+    A.sort()
+    break_point = 0
 
-    for i in range(len(A)-2):
+    for i in range(N-2):
+
         if A[i] < 0:
             pass
         else:
             if (A[i] + A[i+1] > A[i+2]):
-                return 1
+                break_point = 1
                 break
     
-    return 0
+    if break_point == 1:
+        return 1
+    else:
+        return 0
+
 
 if __name__ == '__main__':
     A_1 = [10, 2, 5, 1, 8, 20]
