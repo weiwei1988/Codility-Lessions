1a1713ee1a392dc93ce4135a13fe60e13f498a21
weiwei1988 <zhaoweiheng8828@gmail.com>
2018-10-23 06:35:11 +0900

    add files

--- CodingTest_1.py
+++ CodingTest_1.py
@@ -10,29 +10,22 @@ def solution(A, B, M, X, Y):
         people_weight += A[i]
         go_floor.append(B[i])
 
-        if people_N == X:
-            N_floor += len(set(go_floor)) + 1
-            print('a', go_floor)
-            people_N, people_weight, go_floor = reset(people_N, people_weight, go_floor)
 
-        elif people_weight == Y:
-            N_floor += len(set(go_floor)) + 1
-            print('b', go_floor)
-            people_N, people_weight, go_floor = reset(people_N, people_weight, go_floor)
-        
-        elif people_weight > Y:
-            people_N = people_N - 1
-            people_weight = people_weight - A[i]
-            del go_floor[-1]
+        if people_N == X or people_weight >= Y:
+            if people_N == X or people_weight == Y:
+                N_floor += len(set(go_floor)) + 1
+                people_N, people_weight, go_floor = reset(people_N, people_weight, go_floor)
+            else:
+                people_N -= 1
+                people_weight -= A[i]
+                del go_floor[-1]
+                i -= 1
 
-            N_floor += len(set(go_floor)) + 1
-            print('c', go_floor)
-            people_N, people_weight, go_floor = reset(people_N, people_weight, go_floor)
-            i = i - 1
+                N_floor += len(set(go_floor)) + 1
+                people_N, people_weight, go_floor = reset(people_N, people_weight, go_floor)
 
         elif i == len(A) - 1:
             N_floor += len(set(go_floor)) + 1
-            print('d', go_floor)
             people_N, people_weight, go_floor = reset(people_N, people_weight, go_floor)
             break
         
@@ -48,19 +41,24 @@ def reset(people_N, people_weight, go_floor):
     return people_N, people_weight, go_floor
 
 if __name__ == "__main__":
+    """乗る人の体重"""
     A = [100, 100, 50, 50, 40]
     A_2 = [60, 80, 40]
     A_3 = [30]
 
-    B = [1, 2, 3, 4, 5]
-    B_2 = [2, 3, 5]
+    """行きたい階"""
+    B = [2, 2, 3, 2, 3]
+    B_2 = [2, 3, 3]
 
+    """階の数"""
     M = 3
     M_2 = 5
 
+    """人数制限"""
     X = 5
     X_2 = 2
 
+    """体重制限"""
     Y = 200
     Y_2 = 200
 
---
+++ test.py
@@ -0,0 +1,5 @@
+for i in range(101):
+
+   if i % 4 == 0:
+       print(i)
+       continue
